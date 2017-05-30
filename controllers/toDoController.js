23f6edc5c8dcd87799185b0319069cc1ea3e8296
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-05-30 15:28:50 +0200

    tinkering

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 
 	app.get('/todo', function(req, res) {
-		res.render('todo');
+    	res.sendFile(__dirname + '/../views/todo.html');
 	});
 
 	app.post('/todo', function(req, res) {
--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 let express = require('express');
 let toDoController = require('./controllers/toDoController');
-
+var engine = require('ejs');
 let app = express();
 
 // set up template engine
+//app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
 // Static files
-//app.use(express.static('public'));
+app.use(express.static('./public'));
 
 // Fire controllers
-//toDoController(app);
-
+// toDoController(app);
 app.get('/todo', function(req, res) {
-	res.sendFile(__dirname + '/views/todo.html');
-});
-
-app.post('/todo', function(req, res) {
-
-});
-
-app.delete('/todo', function(req, res) {
-
+    res.sendFile(__dirname + '/views/todo.html');
 });
 
 // Listen to port
