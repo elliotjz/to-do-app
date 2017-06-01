c2cc816de49cd8e7402d589d5085ef7ea3415750
Elliot Zoerner <eliot.zoerner@gmail.com>
2017-06-01 18:57:24 +0200

    changed url

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,14 +1,43 @@
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+
+// Connect to database
+let url = process.env.MLAB_URL;
+mongoose.connect(url);
+
+// Create a schema - this is like a blueprint
+let todoSchema = new mongoose.Schema({
+	item: String
+});
+
+// Create model
+let Todo = mongoose.model('Todo', todoSchema);
+
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
 
 	app.get('/todo', function(req, res) {
-    	res.sendFile(__dirname + '/../views/todo.html');
+		// Get data from mongodb and pass it to view
+		Todo.find({}, function(err, data) {
+			if (err) throw err;
+			res.render('todo', { todos: data });
+		});
 	});
 
-	app.post('/todo', function(req, res) {
-
+	app.post('/todo', urlencodedParser, function(req, res) {
+		// Get data from the view and add it to mongodb
+		let newTodo = Todo(req.body).save(function(err, data) {
+			if (err) throw err;
+			res.json(data);
+		})
 	});
 
-	app.delete('/todo', function(req, res) {
-
+	app.delete('/todo/:item', function(req, res) {
+		// Delete requested item from mongodb
+		Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+			if (err) throw err;
+			res.json(data);
+		})
 	});
 };
--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 let express = require('express');
 let toDoController = require('./controllers/toDoController');
-var engine = require('ejs');
-let app = express();
+let path = require('path');
+
+const app = express();
 
 // set up template engine
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // Static files
 app.use(express.static('./public'));
 
 // Fire controllers
-// toDoController(app);
-app.get('/todo', function(req, res) {
-    res.sendFile(__dirname + '/views/todo.html');
-});
+toDoController(app);
 
 // Listen to port
-app.listen(3000, function() {
-	console.log('Listning to port 3000');
-});
+app.listen(3000);
+
+console.log('Listning to port 3000');
